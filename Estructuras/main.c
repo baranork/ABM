27047e9e713e78e2c1d1a9d9ec67839f66ad40f1
baranork <baranork00@gmail.com>
2019-04-15 23:31:32 -0300

    Add files via upload

--- Estructuras/main.c
+++ Estructuras/main.c
@@ -19,64 +19,130 @@ int buscarLibre(eEmpleado[], int);
 void inicializarEmpleados(eEmpleado[], int);
 int buscarUno(eEmpleado[], int, int);
 int borrarUno(eEmpleado[], int, int);
-int mostrarMenu(void);
+void mostrarMenu(eEmpleado[], int);
 
 int main()
 {
 
     eEmpleado lista[T];
     inicializarEmpleados(lista, T);
-    mostrarMenu;
+    mostrarMenu(lista, T);
 
 
-    cargarEmpleados(lista, T);
-    mostrarListaEmpleados(lista, T);
+
+
 
     return 0;
 }
 
-int mostrarMenu(void)
+void mostrarMenu(eEmpleado lista[], int tam)
 {
-    printf("1. Cargar Empleado\n2.Buscar empleados\n3.Mostrar lista de empleados\n4.Borrar empleado\n");
+    int opcion;
+    char seguir = 's';
+    do
+    {
+        fflush(stdin);
+        system("cls");
+        printf("1. Cargar Empleado\n2.Buscar empleados\n3.Mostrar lista de empleados\n4.Borrar empleado\n5.Salir\nSeleccione una opcion:\n");
+        scanf("%d", &opcion);
+
+        switch(opcion)
+        {
+        case 1:
+            cargarEmpleados(lista, tam);
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+
+            system("pause");
+            system("cls");
+            break;
+        case 3:
+            mostrarListaEmpleados(lista, T);
+            system("pause");
+            system("cls");
+            break;
+        case 4:
+            system("pause");
+            system("cls");
+            break;
+        case 5:
+            seguir = 'n';
+            break;
+        default:
+
+            break;
+        }
+
+    }
+    while(seguir == 's');
 
 }
+
 
 void inicializarEmpleados(eEmpleado lista[], int tam)
 {
     int i;
-    for(i=0; i<tam; i++){
+    for(i=0; i<tam; i++)
+    {
         lista[i].estado = 0;
     }
 }
 void cargarEmpleados(eEmpleado lista[], int tam)
 {
     int i;
+    int indice;
 
-    for(i=0; i<tam; i++)
-    {
+    indice = buscarLibre(lista, tam);
+
+    if(indice== -1){
+        printf("No se puedo cargar el empleado. Espacio insuficiente");
+    }
+    else{
         printf("Ingrese legajo: ");
-        scanf("%d", &lista[i].legajo);
+        scanf("%d", &lista[indice].legajo);
         printf("Ingrese nombre: ");
         fflush(stdin);
-        gets(lista[i].nombre);
+        gets(lista[indice].nombre);
         printf("Ingrese sexo: ");
         fflush(stdin);
-        scanf("%c", &lista[i].sexo);
+        scanf("%c", &lista[indice].sexo);
         printf("Ingrese sueldo bruto: ");
-        scanf("%f", &lista[i].sueldoBruto);
-        lista[i].sueldoNeto = lista[i].sueldoBruto*0.85;
+        scanf("%f", &lista[indice].sueldoBruto);
+        lista[indice].sueldoNeto = lista[indice].sueldoBruto*0.85;
     }
+
+
 }
 
 void mostrarEmpleado(eEmpleado unEmpleado)
 {
-   printf("%d-%s-%c-%f-%f\n", unEmpleado.legajo, unEmpleado.nombre, unEmpleado.sexo, unEmpleado.sueldoBruto, unEmpleado.sueldoNeto);
+    printf("%d-%s-%c-%f-%f\n", unEmpleado.legajo, unEmpleado.nombre, unEmpleado.sexo, unEmpleado.sueldoBruto, unEmpleado.sueldoNeto);
 }
 
 void mostrarListaEmpleados(eEmpleado lista[], int tam)
 {
     int i;
-    for(i=0; i<T; i++){
+    for(i=0; i<tam; i++)
+    {
         mostrarEmpleado(lista[i]);
     }
+}
+
+int buscarLibre(eEmpleado lista[], int tam)
+{
+    int i;
+    int flag = 0;
+    int indice = -1;
+    for(i=0; i<tam; i++)
+    {
+        if(lista[i].estado == 0){
+            flag = 1;
+            indice = i;
+            break;
+        }
+    }
+
+    return indice;
 }
